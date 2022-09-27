54cf07ab90401f877f1ceec28bef582975f5e277
FernandoLopezz11 <fernandolopezcontact1@gmail.com>
2022-09-26 23:41:53 -0300

    commit

---
+++ LastName_FirstName_JS_CrearVariables.js
@@ -0,0 +1,17 @@
+
+const minHeight = 1;     // altura mínima para subir
+const minAge = 10;     // edad mínima para subir 
+
+const person = {
+    age: Math.random(),
+    height: Math.random()
+}
+
+//se me ocurrio una variable que es un objeto que representa la persona, tiene dos propiedades : edad y altura, que
+// tienen un número aleatorio en su valor. Despues cree dos variables, "minHeight" y "minAge" para determinar la altura mínima
+// para que la persona pueda entrar. 
+//Podriamos configurar por ejemplo un condicional que compare las propiedades del objeto "person" con "minHeight" y "minAge",
+// para saber si la persona cumple con los mínimos estandares.
+  
+
+ 
