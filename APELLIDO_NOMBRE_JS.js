b970b4acf0957128484ccac8fc992624a566a4e1
FernandoLopezz11 <fernandolopezcontact1@gmail.com>
2022-09-29 23:00:56 -0300

    Predictions Completed

---
+++ APELLIDO_NOMBRE_JS.js
@@ -0,0 +1,38 @@
+
+// Prediccion 1 =  
+
+  function myBirthYearFunc(){
+      console.log("Nací en" + 1980);
+    }
+
+    
+    //                 Indicara justo lo que pone: (Nací en 1980)
+
+
+//Prediccion 2 = 
+
+function myBirthYearFunc(birthYearInput){
+    console.log("Nací en" + birthYearInput);
+    }
+    
+// En el codigo no aparece declarado el valor debirthYearInput pero en caso de que lo esté, debería poner :
+// (Nací en 1980)
+
+
+//Predicción 3 = 
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 es: " + num1);
+    console.log("num2 es: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+
+// ¡Sumando números!
+// num1 es: 10
+// num2 es: 20
+// 30
+
+
+
+    
